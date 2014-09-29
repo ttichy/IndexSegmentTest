5e1799ea0b3dff06b7e8e894cfb65196f28ca011
Tom Tichy <ttichy@ra.rockwell.com>
2014-09-29 14:59:08 -0400

    index segment for triangular cals done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,6 +7,36 @@ var indexSegmentApp = angular.module('indexSegmentApp', []);
 indexSegmentApp.controller('IndexCtrl', function ($scope) {
     //preselect index type
     $scope.indexType = 1;
+
+    //calculate velocity position at specified tim
+    $scope.calculate = function (time) {
+
+
+        debugger;
+
+        var tf = parseFloat($scope.tf, 10);
+        var t0 = parseFloat($scope.t0, 10);
+        var v0 = parseFloat($scope.v0, 10);
+        var vf = parseFloat($scope.vf, 10);
+        var s0 = parseFloat($scope.s0, 10);
+        var sf = parseFloat($scope.sf, 10);
+
+        var halfTime = (tf - t0) / 2;
+
+        var A1 = (halfTime - t0) * v0;
+        var A3 = (tf - halfTime) * vf;
+        var A4 = (sf - s0 - A1 - vf * halfTime / 2 + v0 * halfTime / 2 - A3)/2;
+
+        var vm = 2 * A4 / halfTime + vf;
+
+        var A2 = A4 + vf*halfTime/2 -v0*halfTime/2;
+
+
+        var distance = A1 + A2 + A3 + A4;
+
+    }
+
+
 });
 
 
