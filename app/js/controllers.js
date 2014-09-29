bf58f161a8de0baecaf06d677d155daa753917dd
Tom Tichy <ttichy@ra.rockwell.com>
2014-09-29 12:34:59 -0400

    playing with the chart

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,15 +2,27 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var indexSegmentApp = angular.module('indexSegmentApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+indexSegmentApp.controller('IndexCtrl', function ($scope) {
+    //preselect index type
+    $scope.indexType = 1;
 });
+
+
+//phonecatApp.controller('PhoneListCtrl', function ($scope) {
+//    $scope.phones = [
+//      {
+//          'name': 'Nexus S',
+//          'snippet': 'Fast just got faster with Nexus S.'
+//      },
+//      {
+//          'name': 'Motorola XOOM™ with Wi-Fi',
+//          'snippet': 'The Next, Next Generation tablet.'
+//      },
+//      {
+//          'name': 'MOTOROLA XOOM™',
+//          'snippet': 'The Next, Next Generation tablet.'
+//      }
+//    ];
+//});
