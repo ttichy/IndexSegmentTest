9e209c55194b6c16ff71db10ff1ccac6171818c1
Tom Tichy <ttichy@ra.rockwell.com>
2014-09-30 13:47:44 -0400

    trap segment done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,8 +8,8 @@ indexSegmentApp.controller('IndexCtrl', function ($scope) {
 
     //define type enum
     var IndexEnum = Object.freeze({
-        TRIANGULAR: 1,
-        TRAPEZOID: 2,
+        TRIANGULAR: "1",
+        TRAPEZOID: "2",
     })
 
 
@@ -17,7 +17,7 @@ indexSegmentApp.controller('IndexCtrl', function ($scope) {
     var EPSILON = 0.000000000000001; 
 
     //preselect index type
-    $scope.indexType = IndexEnum.TRIANGULAR;
+    $scope.indexType = IndexEnum.TRAPEZOID;
 
     //save typing for testing
     $scope.v0 = '5';
@@ -37,7 +37,7 @@ indexSegmentApp.controller('IndexCtrl', function ($scope) {
             case IndexEnum.TRIANGULAR:
                 return CalculateTriangularSegment();
             case IndexEnum.TRAPEZOID:
-                return CalculateTrapezoidSegment();
+                return CalculateTrapezoidalSegment();
             default:
                 return null;
         }
@@ -140,12 +140,12 @@ indexSegmentApp.controller('IndexCtrl', function ($scope) {
 
 
         //parse input fields as floats
-        var tf = parseFloat($scope.tf, 10);
-        var t0 = parseFloat($scope.t0, 10);
-        var v0 = parseFloat($scope.v0, 10);
-        var vf = parseFloat($scope.vf, 10);
-        var s0 = parseFloat($scope.s0, 10);
-        var sf = parseFloat($scope.sf, 10);
+        var tf = parseFloat($scope.tf, 10);     //final time
+        var t0 = parseFloat($scope.t0, 10);     //initial time
+        var v0 = parseFloat($scope.v0, 10);     //initial velocity
+        var vf = parseFloat($scope.vf, 10);     //final velocity
+        var s0 = parseFloat($scope.s0, 10);     //initial position
+        var sf = parseFloat($scope.sf, 10);     //final position
 
 
         var t1 = (tf + 2 * t0) / 3;
@@ -155,8 +155,102 @@ indexSegmentApp.controller('IndexCtrl', function ($scope) {
         var b = (t2 - t1);
         var c = (tf - t2) / 2;
 
+        var A1 = (t1 - t0) * v0;
+        var A5 = (tf - t2) * vf;
+
+        var A3 = (sf - s0 - A1 + a * v0 + c * vf - A5) / ((a + b + c) / b);
+        var vm = A3 / b;
+
+        var A2 = a * vm - a * v0;
+        var A4 = c * vm - c * vf;
+
+        var distance = A1 + A2 + A3 + A4 + A5;
+        if (Math.abs(distance - (sf - s0)) > EPSILON)
+            throw new Error("calculation did not pass distance check");
+
+        //calculation will be done in thirds
+
+        var duration = tf - t0;
+
+        var increment = duration / resolution;
+
+        //initalize array
+        var result = new Array();
+
+        //calculate acceleration 
+        var a0 = (vm-v0) / (t1 - t0);
+        var t = t0;
+
+        //doing first third
+        while (lessThan(t, t1, EPSILON)) {
+
+            var s = s0 + v0 * t + 0.5 * a0 * Math.pow(t, 2);      // s = s0 + v0t + 1/2 at^2
+            var v = v0 + a0 * t;
+
+            //create time, velocity, distance point
+            var point = [t, v, s];
+
+            //add point to result
+            result.push(point);
+
+            //increment time
+            t += increment;
+        }
+
+        //doing second third, recalculating initial
+        t = t1;
+
+        //initial values in the middle of the triangle
+        var sInit = s0 + v0 * t + 0.5 * a0 * Math.pow(t, 2);
+        var vInit = vm;
+
+        a0 = 0; //this is  a trap
+
+        while (lessThan(t, t2, EPSILON)) {
+
+            var time = t - t1;        //account for time correctly
+
+            var s = sInit + vInit * time + 0.5 * a0 * Math.pow(time, 2);      // s = s0 + v0t + 1/2 at^2
+            var v = vInit + a0 * time;
+
+            //create time, velocity, distance point
+            var point = [t, v, s];
+
+            //add point to result
+            result.push(point);
+
+            //increment time
+            t += increment;
+        }
 
 
+        //doing last third, recalculating initial
+        t = t2;
+
+        //initial values in the middle of the triangle
+        var sInit2 = sInit + vInit * (t-t1) + 0.5 * a0 * Math.pow(t-t1, 2);
+        var vInit2 = vm;
+
+        a0 = (vf - vm) / (tf - t2);
+
+        while (lessThan(t, tf, EPSILON)) {
+
+            var time = t - t2;        //account for time correctly
+
+            var s = sInit2 + vInit2 * time + 0.5 * a0 * Math.pow(time, 2);      // s = s0 + v0t + 1/2 at^2
+            var v = vInit2 + a0 * time;
+
+            //create time, velocity, distance point
+            var point = [t, v, s];
+
+            //add point to result
+            result.push(point);
+
+            //increment time
+            t += increment;
+        }
+
+        return result;
 
     }
 
